319a53fa1d0979a7b5f9214e1e2b221ded2bb482
Arleta <laililang@gmail.com>
2018-06-07 00:24:40 +0200

    5 screen in progress

--- app/screens/controllers/screen05.js
+++ app/screens/controllers/screen05.js
@@ -1,30 +1,61 @@
 myApp.controller('screen05Controller', ['$scope', function($scope){
 
     $scope.drags = [
-        {name: "dobré odpoledne", category: "greetings", moved: false},
-        {name: "čau", category: "goodbyes", moved: false},
-        {name: "prosím", category: "behaviours", moved: false},
-        {name: "dobrý večer", category: "greetings", moved: false},
-        {name: "sbohem", category: "goodbyes", moved: false},
-        {name: "děkuji", category: "behaviours", moved: false},
-        {name: "na shledanou", category: "goodbyes", moved: false}
+        {title: "dobré odpoledne", category: "greetings", name: "d1"},
+        {title: "čau", category: "goodbyes", name: "d2"},
+        {title: "prosím", category: "behaviours", name: "d3"},
+        {title: "dobrý večer", category: "greetings", name: "d4"},
+        {title: "sbohem", category: "goodbyes", name: "d5"},
+        {title: "děkuji", category: "behaviours", name: "d6"},
+        {title: "na shledanou", category: "goodbyes", name: "d7"}
     ]
 
-    $scope.holders = ["Powitania", "Pożegnania", "Zwroty grzecznościowe"];
+    $scope.checkingPoints = document.getElementsByClassName('checkingPoints');
+    $scope.points = 0;
+    $scope.disableBtn = true;
 
-    $scope.elements = document.getElementsByClassName('dragables')[0];
-    $scope.fields = document.getElementsByClassName('holders');
+    $scope.checkCategory = function(event) {
 
-    for($scope.field of $scope.fields) {
-        $scope.field.addEventListener("dragover", function(e) {
-            e.preventDefault();
-        });
-        $scope.field.addEventListener("dragenter", function(e) {
-            e.preventDefault();
-        });
-        $scope.field.addEventListener("drop", function() {
-            this.append(e.target);
-        });
-      }
+        event.target.checked = true;
+        console.log(event);
+
+        var tempName = event.target.name;
+        var name = document.getElementsByName(tempName);
+
+        for(var i = 0; i < name.length; i++) {
+            if(name[i].id != event.target.id) {
+                name[i].checked = false;
+            }
+        }
+        
+        $scope.points = 0;
+        $scope.countArray = [];
+        
+        // for(var i = 0; i < $scope.checkingPoints.length; i++) {
+        //     if($scope.checkingPoints[i].checked == true) {
+        //         $scope.countArray.push($scope.checkingPoints[i]);
+        //         $scope.points++;
+        //     }
+        // }
+
+        // if($scope.points == $scope.sentences.length) {
+        //     $scope.disableBtn = false;
+        // }
+
+    }
+
+    // $scope.checkExercise = function() {
+
+    //     for(var i = 0; i < $scope.helperArray.length; i++) {
+    //         if($scope.helperArray[i].value == $scope.sentences[i].result) {
+    //             console.log('Done!');
+    //             $scope.points++;
+    //         }
+    //         else {
+    //             console.log('Wrong!');
+    //         }
+    //     }
+        
+    // }
 
 }]);
