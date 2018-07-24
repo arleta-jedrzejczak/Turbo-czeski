550f42763158eed933ec9df47e0cda9ad3963a0c
Arleta <laililang@gmail.com>
2018-07-25 00:35:03 +0200

    done

--- app/screens/controllers/screen05.js
+++ app/screens/controllers/screen05.js
@@ -3,12 +3,12 @@ myApp.controller('screen05Controller', ['$scope', function($scope){
     let currentScreen = 5;
 
     $scope.drags = [
-        {title: "dobré odpoledne", category: "greetings", name: "d1"},
         {title: "čau", category: "goodbyes", name: "d2"},
-        {title: "prosím", category: "behaviours", name: "d3"},
-        {title: "dobrý večer", category: "greetings", name: "d4"},
+        {title: "dobré odpoledne", category: "greetings", name: "d1"},
         {title: "sbohem", category: "goodbyes", name: "d5"},
         {title: "děkuji", category: "behaviours", name: "d6"},
+        {title: "prosím", category: "behaviours", name: "d3"},
+        {title: "dobrý večer", category: "greetings", name: "d4"},
         {title: "na shledanou", category: "goodbyes", name: "d7"}
     ]
 
@@ -59,5 +59,29 @@ myApp.controller('screen05Controller', ['$scope', function($scope){
         }
         $scope.checkTotalPoints($scope.points, currentScreen);
     }
+
+
+
+    const screen5Bg = document.getElementById('screen5Bg');
+    const notepad = document.getElementById('notepad5');
+    const text = document.getElementById('text5');
+    const drag = document.getElementById('drag5');
+
+    var timeline5 = new TimelineMax({onComplete: $scope.accentChevron});
+
+    TweenMax.set(screen5Bg, {opacity: 0});
+    TweenMax.set(notepad, {opacity: 0, x: 500});
+    TweenMax.set(text, {opacity: 0, y: 300});
+    TweenMax.set(drag, {opacity: 0, scale: 10});
+
+    TweenMax.to(screen5Bg, 1, { opacity: 1 });
+
+    // timeline5.to(screen5Bg, 2, {opacity: 0, display: 'none', ease:Power4.easeInOut}, 1);
+    // timeline5.to(notepad, 2, {opacity: 1, ease: RoughEase.ease.config({ template:  Power0.easeNone, strength: 1, points: 20, taper: "none", randomize: true, clamp: false}), x: 0});
+    // timeline5.to(text, 1, {opacity: 1, y: 0, ease:Power4.easeInOut});
+    // timeline5.to(text, 1, {opacity: 0, y: 200, ease:Power4.easeInOut}, "+=5");
+    // timeline5.to(notepad, 1, {opacity: 0, ease:Power4.easeInOut}, '+=0.5');
+    // timeline5.to(screen5Bg, 2, {opacity: 1, display: 'block', ease:Power4.easeInOut});
+    timeline5.to(drag, 1, {opacity: 1, scale: 1, ease: Bounce.easeOut});
 
 }]);
